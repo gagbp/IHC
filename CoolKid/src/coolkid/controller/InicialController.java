e537a21e4dfba60d4e246d5fc8d1666270bd2c3a
Gabriel Brunelli Pereira <30929090+gagbp@users.noreply.github.com>
2018-05-29 00:19:21 -0300

    IHC

---
+++ CoolKid/src/coolkid/controller/InicialController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coolkid.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author MP
+ */
+public class InicialController implements Initializable {
+
+    @FXML
+    private void Cadastro(ActionEvent event) {
+        System.out.println("Cadastro");
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
